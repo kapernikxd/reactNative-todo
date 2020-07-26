617908a7b6598f890335ff1e764cb0a5aff043be
kapernikxd <you@example.com>
2020-07-26 14:47:34 +0300

    2 основы

--- App.js
+++ App.js
@@ -1,21 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react'
+import { StyleSheet, Text, View, FlatList } from 'react-native'
+import { Navbar } from './src/Navbar'
+import { AddTodo } from './src/AddTodo'
+import {Todo} from './src/Todo'
 
 export default function App() {
+  const [todos, setTodos] = useState([
+    { id: 1, title: 'test' },
+    { id: 2, title: 'test' },
+    { id: 3, title: 'test' },
+    { id: 4, title: 'test' },
+    { id: 5, title: 'test' },
+    { id: 6, title: 'test' },
+    { id: 7, title: 'test' }
+  ])
+
+  const addTodo = title => {
+    // const newTodo = {
+    //   id: Date.now().toString(),
+    //   title: title
+    // }
+
+  
+
+    // setTodos(todos.concat([ newTodo ]))
+    // setTodos((prevTodos) => {
+    //   return [
+    //     ...prevTodos,
+    //     newTodo
+    //   ]
+    // })
+
+    setTodos(prev => [
+      ...prev,
+      {
+        id: Date.now().toString(),
+        title
+      }
+    ])
+  }
+
+  const removeTodo = id => {
+    setTodos(prev => prev.filter(todo => todo.id !== id))
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Navbar title='Todo Apps!' />
+      <AddTodo onSubmit={addTodo}/>
+
+      <FlatList
+          keyExtractor={item => item.id.toString()}
+          data={todos}
+          renderItem={({ item }) => <Todo todo={item} onRemove={removeTodo} />}
+        />
+
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  container: {}
+})
---
+++ src/AddTodo.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react'
+import { View, StyleSheet, TextInput, Button, Alert } from 'react-native'
+
+export const AddTodo = ({ onSubmit }) => {
+  const [value, setValue] = useState('')
+
+  const pressHandler = () => {
+    if (value.trim()) {
+      onSubmit(value)
+      setValue('')
+    } else {
+      Alert.alert('Названия дела не может быть пустым')
+    }
+  }
+
+  return (
+    <View style={styles.block}>
+      <TextInput
+        style={styles.input}
+        onChangeText={setValue}
+        value={value}
+        placeholder='Введите название дела...'
+        autoCorrect={false}
+        autoCapitalize='none'
+      />
+      <Button title='Добавить' onPress={pressHandler} />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  block: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    marginTop: 10,
+  },
+  input: {
+    width: '60%',
+    padding: 10,
+    borderStyle: 'solid',
+    borderBottomWidth: 2,
+    borderBottomColor: '#3949ab',
+    fontSize:24,
+    
+  },
+
+})
